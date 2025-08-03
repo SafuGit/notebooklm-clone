3e61620dd2a69c3f334383c8f5ac7b9a883aaddc
Safwan Sadid <98327221+SafuGit@users.noreply.github.com>
2025-08-03 13:55:34 +0600

    add multi page system

--- src/components/PdfDoc.tsx
+++ src/components/PdfDoc.tsx
@@ -2,20 +2,38 @@ import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 import { PdfContext } from '../providers/PdfContext';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
+import { ArrowLeft, ArrowRight } from 'lucide-react';
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   'pdfjs-dist/build/pdf.worker.min.mjs',
-  import.meta.url,
+  import.meta.url
 ).toString();
 
 const PdfDoc = () => {
   const { pdfFile: pdf } = useContext(PdfContext);
+  const [numPages, setNumPages] = useState<number>(0);
+  const [pageNumber, setPageNumber] = useState<number>(1);
+
+  const onDocumentLoadSuccess = ({ numPages }: { numPages: number }) => {
+    setNumPages(numPages);
+    setPageNumber(1);
+  };
+
+  const goToPrevPage = () => setPageNumber((prev) => Math.max(prev - 1, 1));
+  const goToNextPage = () => setPageNumber((prev) => Math.min(prev + 1, numPages));
 
   return (
     <div>
-      <Document file={pdf || ''} className={'w-[40%]'} >
-        <Page pageNumber={1} />
+      <Document file={pdf || ''} className={'w-[40%]'} onLoadSuccess={onDocumentLoadSuccess} >
+        <div className='flex items-center'>
+          <button className='btn btn-primary btn-circle btn-ghost' onClick={goToPrevPage}><ArrowLeft /></button>
+          <span className='mx-2'>
+            Page {pageNumber} of {numPages}
+          </span>
+          <button className='btn btn-primary btn-circle btn-ghost' onClick={goToNextPage}><ArrowRight /></button>
+        </div>
+        <Page pageNumber={pageNumber} />
       </Document>
     </div>
   );
