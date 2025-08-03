c1f391330dbdc61b2e44aff1a38b64c07093a26f
Safwan Sadid <98327221+SafuGit@users.noreply.github.com>
2025-08-03 19:30:48 +0600

    improve chat

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,3 +1,5 @@
+import Markdown from "react-markdown";
+
 const Chat = ({messages}: {messages: {role: string, content: string}[]}) => {
   console.log(messages);
   return (
@@ -8,7 +10,7 @@ const Chat = ({messages}: {messages: {role: string, content: string}[]}) => {
             {msg.role === 'user' ? 'You' : 'PDF Reader AI'}
             <time className="text-xs opacity-50">{new Date().toLocaleTimeString()}</time>
           </div>
-          <div className="chat-bubble">{msg.content}</div>
+          <div className="chat-bubble"><Markdown>{msg.content}</Markdown></div>
           <div className="chat-footer opacity-50">Seen</div>
         </div>
       ))}
